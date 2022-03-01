4271dda0bc8c2a5a963cda1def57ac1ba4576b8a
NilambariPatil15 <patilnilambarid15498@gmail.com>
2022-03-01 21:59:14 +0530

    first commit

---
+++ src/main/java/com/demo1/spring_boot_demo/SpringJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.demo1.spring_boot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsDockerApplication.class, args);
+	}
+
+}
